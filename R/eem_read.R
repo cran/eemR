6e1abb44ae0a052cf98ca1c54020a9ce4af913df
Philippe Massicotte <pm@bios.au.dk>
2015-11-19 17:22:00 +0000

    version 0.1.0

---
+++ R/eem_read.R
@@ -0,0 +1,236 @@
+#' Read excitation-emission fluorecence matrix (eem)
+#'
+#' @param file File name or folder containing fluorescence file(s).
+#' @param recursive logical. Should the listing recurse into directories?
+#'
+#' @return If \code{file} is a single filename:
+#'
+#'   An object of class \code{eem} containing: \itemize{ \item sample The file
+#'   name of the eem. \item x A matrix with fluorescence values. \item em
+#'   Emission vector of wavelengths. \item ex Excitation vector of wavelengths.
+#'   }
+#'
+#'   If \code{file} is a folder, the function returns an object of class
+#'   \code{eemlist} which is simply a list of \code{eem}.
+#'
+#' @details At the moment, Cary Eclipse, Aqualog and Shimadzu EEMs are
+#'   supported.
+#'
+#'   \code{eemR} will automatically try to determine from which
+#'   spectrofluorometer the files originate and load the data accordingly. Note
+#'   that EEMs are reshaped so X[1, 1] represents the fluoresence intensity at
+#'   X[min(ex), min(em)].
+#'
+#' @importFrom stats na.omit
+#' @export
+#' @examples
+#' file <- system.file("extdata/cary/eem/", "sample1.csv", package = "eemR")
+#' eem <- eem_read(file)
+
+eem_read <- function(file, recursive = FALSE) {
+
+  stopifnot(file.exists(file) | file.info(file)$isdir,
+            is.logical(recursive))
+
+  #--------------------------------------------
+  # Verify if user provided a dir or a file.
+  #--------------------------------------------
+  isdir <- file.info(file)$isdir
+
+  if(isdir){
+
+    files <- list.files(file, full.names = TRUE, recursive = recursive,
+                        no.. = TRUE, include.dirs = FALSE)
+
+    files <- files[!file.info(files)$isdir]
+
+    res <- lapply(files, eem_read)
+
+    #res <- lapply(res, my_unlist)
+    #res <- unlist(res, recursive = FALSE)
+
+    class(res) <- "eemlist"
+
+    res[unlist(lapply(res, is.null))] <- NULL ## Remove unreadable EEMs
+
+    return(res)
+  }
+
+  #---------------------------------------------------------------------
+  # Read the file and try to figure from which spectrofluo it belongs.
+  #---------------------------------------------------------------------
+  #data <- readLines(file)
+
+  data <- read_lines(file)
+
+  if(is_cary_eclipse(data)){
+    return(eem_read_cary(data, file))
+  }
+
+  if(is_aqualog(data)){
+    return(eem_read_aqualog(data, file))
+  }
+
+  if(is_shimadzu(data)){
+    return(eem_read_shimadzu(data, file))
+  }
+
+  message("I do not know how to read *** ", basename(file), " ***\n")
+
+  return(NULL)
+
+}
+
+#' eem constructor
+#'
+#' @param sample A string containing the file name of the eem.
+#' @param x A matrix with fluorescence values.
+#' @param ex Vector of excitation wavelengths.
+#' @param em Vector of emission wavelengths.
+#'
+#' @return An object of class \code{eem} containing:
+#' \itemize{
+#'  \item sample The file name of the eem.
+#'  \item x A matrix with fluorescence values.
+#'  \item em Emission vector of wavelengths.
+#'  \item ex Excitation vector of wavelengths.
+#' }
+
+eem <- function(sample, x, ex, em){
+
+  eem <- list(sample = strsplit(basename(sample), "\\.")[[1]][1],
+              x = x,
+              ex = ex,
+              em = em)
+
+  class(eem) <- "eem"
+
+  return(eem)
+}
+
+
+is_cary_eclipse <- function(x) {
+  any(grepl("EX_", x)) ## Need to be more robust
+}
+
+is_aqualog <- function(x) {
+  any(grepl("Normalized by|^Sample - Blank|^Wavelength", x))
+}
+
+is_shimadzu <- function(x){
+
+  x <- stringr::str_split(x, "\t")
+
+  # a bit weak, but works for now
+  all(unlist(lapply(x, length)) %in% 2)
+}
+
+#---------------------------------------------------------------------
+# Function reading Shimadzu .TXT files.
+#---------------------------------------------------------------------
+eem_read_shimadzu <- function(data, file){
+
+  data <- stringr::str_split(data, "\t")
+
+  data <- lapply(data, as.numeric)
+
+  data <- do.call(rbind, data)
+
+  min_em <- min(data[, 1])
+  max_em <- max(data[, 1])
+
+  interval <- data[2, 1] - data[1, 1]
+
+  em <- seq(min_em, max_em, by = interval)
+
+  data <- data[, 2]
+
+  eem <- matrix(data, nrow = length(em), byrow = FALSE)
+
+  ## Construct an eem object.
+  res <- eem(sample = file,
+             x = eem,
+             ex = NA,
+             em = em)
+
+  attr(res, "is_blank_corrected") <- FALSE
+  attr(res, "is_scatter_corrected") <- FALSE
+  attr(res, "is_ife_corrected") <- FALSE
+  attr(res, "is_raman_normalized") <- FALSE
+  attr(res, "manucafturer") <- "Shimadzu"
+
+  message("Shimadzu files do not contain excitation wavelengths.")
+  message("Please provide them using the eem_set_wavelengths() function.")
+
+  return(res)
+
+}
+
+#---------------------------------------------------------------------
+# Function reading Cary Eclipse csv files.
+#---------------------------------------------------------------------
+eem_read_cary <- function(data, file){
+
+  data <- stringr::str_split(data, ",")
+
+  ## Find the probable number of columns
+  expected_col <- length(data[[1]])
+
+  data[lapply(data, length) != expected_col] <- NULL
+
+  ex <- as.numeric(na.omit(stringr::str_match(data[[1]],
+                                              "EX_(\\d{3}.\\d{2})")[, 2]))
+
+  data[1:2] <- NULL ## Remove the first 2 header lines
+
+  data <- matrix(as.numeric(unlist(data, use.names = FALSE)),
+                 ncol = expected_col, byrow = TRUE)
+
+  data <- data[,which(colMeans(is.na(data)) < 1)] ## remove na columns
+
+  eem <- data[, !data[1, ] %in% ex] ## Remove duplicated columns
+
+  em <- data[, 1]
+
+  ## Construct an eem object.
+  res <- eem(sample = file,
+             x = eem,
+             ex = ex,
+             em = em)
+
+  attr(res, "is_blank_corrected") <- FALSE
+  attr(res, "is_scatter_corrected") <- FALSE
+  attr(res, "is_ife_corrected") <- FALSE
+  attr(res, "is_raman_normalized") <- FALSE
+  attr(res, "manucafturer") <- "Cary Eclipse"
+
+  return(res)
+}
+
+#---------------------------------------------------------------------
+# Fonction reading Aqualog dat files.
+#---------------------------------------------------------------------
+eem_read_aqualog <- function(data, file){
+
+  data <- readr::read_delim(file, delim = "\t")
+  data <- na.omit(data)
+
+  ex <- rev(as.numeric(grep("[0-9]", names(data), value = TRUE)))
+  em <- as.numeric(grep("[0-9]", t(data[, 1]), value = TRUE))
+
+  eem <- as.matrix(data[, ncol(data): 2])
+
+  ## Construct an eem object.
+  res <- eem(sample = file,
+             x = eem,
+             ex = ex,
+             em = em)
+
+  attr(res, "is_blank_corrected") <- FALSE
+  attr(res, "is_scatter_corrected") <- FALSE
+  attr(res, "is_ife_corrected") <- FALSE
+  attr(res, "is_raman_normalized") <- FALSE
+  attr(res, "manucafturer") <- "Aqualog"
+
+  return(res)
+}
---
+++ R/eem_utils.R
@@ -0,0 +1,309 @@
+#' Surface plot of eem
+#'
+#' @param x An object of class \code{eem}.
+#' @param ... Extra arguments for \code{image.plot}.
+#' @importFrom grDevices colorRampPalette
+#' @export
+#' @examples
+#' file <- system.file("extdata/cary/eem/", "sample1.csv", package = "eemR")
+#' eem <- eem_read(file)
+#'
+#' plot(eem)
+
+plot.eem <- function(x, ...){
+
+  jet.colors <- colorRampPalette(c("#00007F",
+                                   "blue",
+                                   "#007FFF",
+                                   "cyan",
+                                   "#7FFF7F",
+                                   "yellow",
+                                   "#FF7F00",
+                                   "red",
+                                   "#7F0000"))
+
+  fields::image.plot(y = x$em,
+             x = x$ex,
+             z = t(x$x),
+             main = paste(x$sample, "\n", attr(x, "manucafturer"), sep = ""),
+             xlab = "Excitation (nm.)",
+             ylab = "Emission (nm.)",
+             legend.lab = "Fluorescence intensity",
+             col = jet.colors(255))
+}
+
+#' Surface plot of eem
+#'
+#' @param x An object of class \code{eemlist}.
+#' @param which An integer representing the index of eem to be plotted.
+#' @param ... Extra arguments for \code{image.plot}.
+#'
+#' @export
+#' @examples
+#' folder <- system.file("extdata/cary/eem/", package = "eemR")
+#' eem <- eem_read(folder)
+#'
+#' plot(eem, which = 2)
+plot.eemlist <- function(x, which = 1, ...) {
+
+  stopifnot(which <= length(x))
+
+  plot.eem(x[[which]])
+
+}
+
+#' Display summary of an eem object
+#'
+#' @param object An object of class \code{eem}.
+#' @param ... Extra arguments.
+#'
+#' @references \url{http://www.sciencedirect.com/science/article/pii/0304420395000623}
+#' @importFrom utils head tail
+#' @export
+#' @examples
+#' file <- system.file("extdata/cary/eem/", "sample1.csv", package = "eemR")
+#' eem <- eem_read(file)
+#'
+#' summary(eem)
+
+summary.eem <- function(object, ...){
+
+  stopifnot(class(object) == "eem")
+
+  cat("eem object:", dim(object$x)[1],
+      "x",  dim(object$x)[2],
+      "(", dim(object$x)[1] * dim(object$x)[2], ")", "\n")
+
+  cat("ex: (", range(object$ex), "nm.)", head(object$ex, 3), "...", tail(object$ex, 3), "\n")
+
+  cat("em: (", range(object$em), "nm.)", head(object$em, 3), "...", tail(object$em, 3), "\n")
+
+  cat("is_blank_corrected:", attr(object, "is_blank_corrected"), "\n")
+
+  cat("is_scatter_corrected:", attr(object, "is_scatter_corrected"), "\n")
+
+  cat("is_ife_corrected:", attr(object, "is_ife_corrected"), "\n")
+
+  cat("is_raman_normalized:", attr(object, "is_raman_normalized"), "\n")
+
+  cat("manucafturer:", attr(object, "manucafturer"), "\n")
+}
+
+
+#' Display summary of an eemlist object
+#'
+#' @param object An object of class \code{eemlist}.
+#' @param ... Extra arguments.
+#'
+#' @export
+#' @examples
+#' folder <- system.file("extdata/cary/eem/", package = "eemR")
+#' eem <- eem_read(folder)
+#'
+#' summary(eem)
+summary.eemlist <- function(object, ...){
+
+  stopifnot(class(object) == "eemlist")
+
+  cat("eemlist object containing:", length(object), "eem\n\n")
+
+  cat("First eem object:\n\n")
+  summary.eem(object[[1]])
+}
+
+
+#' Cut emission and/or excitation wavelengths from EEMs
+#'
+#' @template template_eem
+#' @param ex A numeric vector of excitation wavelengths to be removed.
+#' @param em A numeric vector of emission wavelengths to be removed.
+#'
+#' @export
+#' @examples
+#' # Open the fluorescence eem
+#' file <- system.file("extdata/cary/eem/", "sample1.csv", package = "eemR")
+#'
+#' eem <- eem_read(file)
+#' plot(eem)
+#'
+#' # Cut few excitation wavelengths
+#' eem <- eem_cut(eem, ex = c(220, 225, 230, 230))
+#' plot(eem)
+eem_cut <- function(eem, ex, em){
+
+  stopifnot(class(eem) == "eem" | any(lapply(eem, class) == "eem"))
+
+  ## It is a list of eems, then call lapply
+  if(any(lapply(eem, class) == "eem")){
+
+    res <- lapply(eem, eem_cut, ex = ex, em = em)
+
+    class(res) <- class(eem)
+
+    return(res)
+
+  }
+
+  ## Maybe round em and ex wavelengths so it is
+
+  if(!missing(ex)){
+
+    stopifnot(all(ex %in% eem$ex))
+
+    index <- !eem$ex %in% ex
+
+    eem$ex <- eem$ex[index]
+
+    eem$x <- eem$x[, index]
+
+  }
+
+  if(!missing(em)){
+
+    stopifnot(all(em %in% eem$em))
+
+    index <- !eem$em %in% em
+
+    eem$em <- eem$em[index]
+
+    eem$x <- eem$x[index, ]
+  }
+
+  return(eem)
+
+}
+
+#' Set Excitation and/or Emission wavelengths
+#'
+#' This function allows to manully specify either excitation or emission vector
+#' of wavelengths in EEMs. This function is mostly used with spectrophotometers
+#' such as Shimadzu that do not include excitation wavelengths in fluorescence
+#' files.
+#'
+#' @template template_eem
+#' @param ex A numeric vector of excitation wavelengths.
+#' @param em A numeric vector of emission wavelengths.
+#'
+#' @examples
+#' folder <- system.file("extdata/shimadzu", package = "eemR")
+#'
+#' eem <- eem_read(folder)
+#' eem <- eem_set_wavelengths(eem, ex = seq(230, 450, by = 5))
+#'
+#' plot(eem)
+#'
+#' @export
+
+eem_set_wavelengths <- function(eem, ex, em){
+
+  stopifnot(class(eem) == "eem" | any(lapply(eem, class) == "eem"))
+
+  ## It is a list of eems, then call lapply
+  if(any(lapply(eem, class) == "eem")){
+
+    res <- lapply(eem, eem_set_wavelengths, ex = ex, em = em)
+
+    class(res) <- class(eem)
+
+    return(res)
+
+  }
+
+  if(!missing(ex)){
+
+    stopifnot(is.vector(ex),
+              is.numeric(ex),
+              identical(length(ex), ncol(eem$x)),
+              all(ex == cummax(ex))) ## Monotonously increasing
+
+    eem$ex <- ex
+  }
+
+  if(!missing(em)){
+
+    stopifnot(is.vector(em),
+              is.numeric(em),
+              identical(length(em), nrow(eem$x)),
+              all(em == cummax(em))) ## Monotonously increasing
+
+    eem$em <- em
+  }
+
+  return(eem)
+
+}
+
+#' Extract EEM samples
+#'
+#' @template template_eem
+#' @param sample Either numeric of character vector. See \code{details} for more
+#'   information.
+#'
+#' @param remove logical. Should EEMs removed (TRUE) or extracted (FALSE).
+#'
+#' @details \code{sample} argument can be either numeric or character vector. If
+#'   it is numeric, samples at specified index will be removed.
+#'
+#'   If \code{sample} is character, regular expression will be used and all
+#'   sample names that have a partial or complete match with the expression will
+#'   be removed. See \code{examples} for more details.
+#'
+#' @importFrom stringr str_detect
+#'
+#' @examples
+#' folder <- system.file("extdata/cary/eem", package = "eemR")
+#' eems <- eem_read(folder)
+#'
+#' eem_extract(eems, c(1, 3)) ## Removes samples 1 and 3
+#' eem_extract(eems, c(1, 3), remove = TRUE) ## extract samples 1 and 3
+#'
+#' ## Remove all samples containing "3" in their names.
+#' eem_extract(eems, "3")
+#'
+#' ## Remove all samples containing either character "s" or character "2" in their names.
+#' eem_extract(eems, c("s", "2"))
+#'
+#' ## Remove all samples containing "blank" or "nano"
+#' eem_extract(eems, c("blank", "nano"))
+#'
+#' ## Remove all samples starting with "no"
+#' eem_extract(eems, "^no")
+#'
+#' @export
+eem_extract <- function(eem, sample, remove = FALSE) {
+
+  stopifnot(class(eem) == "eemlist",
+            is.character(sample) | is.numeric(sample))
+
+  sample_names <- unlist(lapply(eem, function(x){x$sample}))
+
+  ## Sample number
+  if(is.numeric(sample)){
+
+    stopifnot(all(is_between(sample, 1, length(eem))))
+
+    eem[ifelse(remove, -sample, sample)] <- NULL
+
+    cat(ifelse(remove, "Removed sample(s):", "Extracted sample(s):"),
+        sample_names[sample])
+
+  }
+
+  ## Regular expression
+  if(is.character(sample)){
+
+    index <- str_detect(sample_names, paste(sample, collapse = "|"))
+
+    eem[xor(index, !remove)] <- NULL
+
+    if(all(index == FALSE)){
+      cat("Nothing to remove.")
+    }
+    else{
+      cat(ifelse(remove, "Removed sample(s):", "Extracted sample(s):"),
+          sample_names[index])
+    }
+  }
+
+  return(eem)
+}
